512959e39c5f34b2d9cb18d3f7ee436e74986cfe
Vinibatch <vlazarus@hotmail.fr>
2019-06-25 18:33:13 +0200

    second commit

--- routes/index.js
+++ routes/index.js
@@ -5,51 +5,62 @@ var router = express.Router();
 var databike = [{
   nom: "Model BIKO45",
   photo: "/images/bike-1.jpg",
-  prix: "679€",
+  prix: "679",
 },
 {
   nom: "Model Z00K7",
   photo: "/images/bike-2.jpg",
-  prix: "799€",
+  prix: "799",
 },
 {
   nom: "Model LIK089",
   photo: "/images/bike-3.jpg",
-  prix: "839€",
+  prix: "839",
 },
 {
   nom: "Model GEWO",
   photo: "/images/bike-4.jpg",
-  prix: "1206€",
+  prix: "1206",
 },
 {
   nom: "Model TITAN5",
   photo: "/images/bike-5.jpg",
-  prix: "989€",
+  prix: "989",
 },
 {
   nom: "Model AMIG39",
   photo: "/images/bike-6.jpg",
-  prix: "599€",
+  prix: "599",
 }
 ];
 
 
-var dataCardBike = [
-  {name: 'Model BIKO45', price: 679, url: '/images/bike-1.jpg', quantity: 4},
-  {name: 'Model ZOOK7', price: 799, url: '/images/bike-2.jpg', quantity: 5 }
-]
+var dataCardBike = [];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { databike });
-
+  res.render('index', {databike} );
 });
 
-router.get('/shop', function(req, res, next) {
-  res.render('shop', {dataCardBike}
-  );
+router.post('/shop', function(req, res, next) {
+
+  dataCardBike.push (
+    {
+    nom : req.body.bikeNomFromFront,
+    prix  :  req.body.bikePrixFromFront, 
+    photo : req.body.bikePhotoFromFront, 
+    quantity : req.body.bikeQuantityFromFront
+  });
+
+  res.render('shop', {dataCardBike});
 });
+
+
+router.get('/delete-shop', function (req,res,next) {
+  dataCardBike.slice(req.query.position,1);
+  res.render("shop", {dataCardBike});
+});
+
 
 
 module.exports = router;
